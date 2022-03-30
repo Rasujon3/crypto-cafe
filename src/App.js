d9bff78da8c99fcd42820a032c3429f1df81360e
Rasujon3 <rasujon3@gmail.com>
2022-03-30 21:22:14 +0600

    coin details page done

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Homepage from "./components/Homepage/Homepage";
 import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
 import About from "./components/About/About";
+import CoinDetails from "./components/CoinDetails/CoinDetails";
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/coins" element={<Coins />} />
+        <Route path="/coin-details/:id" element={<CoinDetails />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/about" element={<About />} />
       </Routes>
